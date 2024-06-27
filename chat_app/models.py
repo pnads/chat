5437bb1b548b4a2c622eee0bfd1db6827c9e6674
Patrick Nadeau <patrick.a.nadeau@gmail.com>
2024-06-27 10:53:25 -0400

    Add Message model and migration

--- chat_app/models.py
+++ chat_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    room_name = models.CharField(max_length=255)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"[{self.timestamp}] {self.user.username}: {self.content}"
